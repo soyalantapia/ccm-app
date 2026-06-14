45cae87f7b2754cdf03874d068a7363f715a118b
soyalantapia <alannaimtapia@gmail.com>
2026-06-14 18:02:19 -0300

    Deploy to GitHub Pages

--- assets/chevron-right-DbqHK522.js
+++
@@ -1,1 +0,0 @@
-import{Z as e}from"./index-T7szrX-H.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
--- assets/minus-D2C9JFJn.js
+++
@@ -1,1 +0,0 @@
-import{Z as e}from"./index-T7szrX-H.js";var t=e(`minus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}]]);export{t};
