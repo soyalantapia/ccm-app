9b6847c973af3835151daf838562f0e8f6e3dd50
soyalantapia <alannaimtapia@gmail.com>
2026-06-20 14:44:40 -0300

    feat(membresia): Socio CCM + gate de contenido exclusivo + KPI socios/ingresos en dashboard

---
+++ assets/chevron-left-C9HX9C2R.js
@@ -0,0 +1,1 @@
+import{tt as e}from"./index-CDRx-c3p.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
---
+++ assets/minus-B-aqfvHp.js
@@ -0,0 +1,1 @@
+import{tt as e}from"./index-CDRx-c3p.js";var t=e(`minus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}]]);export{t};
