683a6ae8444525c8b9819ffdb4cc0a4359a5337e
soyalantapia <alannaimtapia@gmail.com>
2026-07-18 22:39:47 -0300

    cazabug: republicar con img/catalogo + fixes

---
+++ assets/chevron-down-BLJSMbyL.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-B3VyxRKw.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
---
+++ assets/minus-FCCXFiyD.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-B3VyxRKw.js";var t=e(`minus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}]]);export{t};
